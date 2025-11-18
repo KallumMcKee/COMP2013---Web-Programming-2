e8c51aac983bf03b19efa54fafcd3a3e475aefe4
Kallum McKee <114624315+KallumMcKee@users.noreply.github.com>
2025-11-17 19:05:36 -0500

    Delete project-1 directory

--- project-1/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import ProductCardContainer from "../Components/ProductCardContainer";
-
-import data from "./data/products";
-
-function App() {
-  return <>{<ProductCardContainer data={data} />}</>;
-}
-export default App;
--- project-1/src/Components/CartCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function CartCard({ product, quantity, currentPrice }) {
-  return (
-    <div className="CartCard">
-      <h2>{product}</h2>
-      <p>Quantity: {quantity}</p>
-      <p>Total Price: ${(quantity * currentPrice).toFixed(2)}</p>
-    </div>
-  );
-}
